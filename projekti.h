d62ad7b1a16a3d3a782724d978703c7f16456e5b
JuanFuentes20 <59918928+JuanFuentes20@users.noreply.github.com>
2020-01-15 20:04:36 +0200

    Add files via upload
    Projektin tietorakenne "olympia"

---
+++ projekti.h
@@ -0,0 +1,17 @@
+//
+//  projekti.h
+//  projekti2
+//
+//  Created by Juan Fuentes on 03/04/2019.
+//  Copyright © 2019 Juan Fuentes. All rights reserved.
+//
+
+#ifndef projekti_h
+#define projekti_h
+
+typedef struct{
+    char maa[20];
+    int kulta,hopea,pronssi;
+} olympia;
+
+#endif /* projekti_h */
